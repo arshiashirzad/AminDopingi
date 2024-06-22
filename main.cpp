bbe92c02c13869f3a0089b3e5f228fe93fff9749
arshiashirzad <sirzad383@gmail.com>
2024-06-22 12:34:57 +0330

    Refreshed Project:Rectangle as a player added

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <QApplication>
-#include "game.h"
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
-Game *game;
+/*
+Prereqs:
+-basic knowledge of c++ (pointers and memory management)
+-VERY basic knowledge of Qt (widgets)
+Tutorial Topics:
+-QGraphicsScene
+-QGraphicsItem (QGraphicsRectItem)
+-QGraphicsView
+*/
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
 
-    game = new Game();
-    game->show();
+    // create a scene
+    QGraphicsScene * scene = new QGraphicsScene();
 
+    // create an item to put into the scene
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0,0,100,100);
+    QBrush redBrush(Qt::red);
+    rect->setBrush(redBrush);
+    // add the item to the scene
+    scene->addItem(rect);
+
+    // add a view to visualize the scene
+    QGraphicsView * view = new QGraphicsView(scene);
+
+    view->show();
     return a.exec();
 }
